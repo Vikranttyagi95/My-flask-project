b887c1dda9a576e97db5ca43823ccbe7a4db26cf
Vityagi <Vikrant.tyg1995@gmail.com>
2021-12-07 10:40:18 +0530

    Modified project structure and added extra files

---
+++ sever.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route("/blog")
+def my_blog():
+    return "This is my blog!"
+
+if __name__ == '__main__':
+    app.run()
