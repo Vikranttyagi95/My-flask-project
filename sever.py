d87279705b48b16e82d9edb7f9275596081b2988
Vityagi <Vikrant.tyg1995@gmail.com>
2021-12-09 21:14:21 +0530

    Modified html files

--- sever.py
+++ sever.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def my_home():
     return render_template('index.html')
 
-@app.route("/blog")
-def my_blog():
-    return "This is my blog!"
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        print(data)
+        return redirect("/thankyou.html")
+    else:
+        return "Something went wrong!"
 
 if __name__ == '__main__':
     app.run()
